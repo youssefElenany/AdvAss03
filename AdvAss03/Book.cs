1450cc2ce1a4d2b9914fc2ce984b7384f058298a
youssefElenany <119540554+youssefElenany@users.noreply.github.com>
2025-02-02 15:32:21 +0200

    Add project files.

---
+++ AdvAss03/Book.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvAss03
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public DateTime PublicationDate { get; set; }
+
+        public decimal Price { get; set; }
+
+        public Book(string title, string author, string isbn, DateTime publicationDate , decimal price)
+        {
+            Title = title;
+            Author = author;
+            ISBN = isbn;
+            PublicationDate = publicationDate;
+            Price = price;
+        }
+        public class BookFunctions
+        {
+            
+            public delegate string BookInfoDelegate(Book book);
+
+            
+            public Func<Book, string> GetAuthor;
+            public Action<Book> DisplayTitle;
+
+            
+            public BookInfoDelegate GetISBN;
+
+            
+            public Func<Book, DateTime> GetPublicationDate;
+
+            public Func<Book, decimal> GetPrice; 
+
+            public BookFunctions()
+            {
+               
+                GetISBN = delegate (Book book)
+                {
+                    return book.ISBN;
+                };
+
+                
+                GetAuthor = (book) => book.Author;
+                DisplayTitle = (book) => Console.WriteLine($"Title: {book.Title}");
+
+                
+                GetPublicationDate = (book) => book.PublicationDate;
+
+                GetPrice = (book) => book.Price;
+
+            }
+        }
+        public class LibraryEngine
+        {
+            private List<Book> books = new List<Book>();
+            private BookFunctions bookFunctions = new BookFunctions();
+
+            public void AddBook(Book book)
+            {
+                books.Add(book);
+            }
+
+            public void DisplayBookDetails()
+            {
+                foreach (var book in books)
+                {
+                    bookFunctions.DisplayTitle(book);
+                    Console.WriteLine($"Author: {bookFunctions.GetAuthor(book)}");
+                    Console.WriteLine($"ISBN: {bookFunctions.GetISBN(book)}");
+                    Console.WriteLine($"Publication Date: {bookFunctions.GetPublicationDate(book):yyyy-MM-dd}");
+                    Console.WriteLine($"Price:{bookFunctions.GetPrice(book)}");
+                    Console.WriteLine(new string('-', 30));
+                }
+            }
+        }
+
+    }
+
+}
---
+++ Ass03/Program.cs
@@ -0,0 +1,22 @@
+﻿using AdvAss03;
+using static AdvAss03.Book;
+
+namespace Ass03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LibraryEngine library = new LibraryEngine();
+
+            Book book1 = new Book("The C# Journey", "John Doe", "123-456-789", new DateTime(2020, 5, 10),3000);
+            Book book2 = new Book("Mastering ASP.NET", "Jane Smith", "987-654-321", new DateTime(2021, 11, 25),4000);
+
+            library.AddBook(book1);
+            library.AddBook(book2);
+
+            library.DisplayBookDetails();
+
+        }
+    }
+}
